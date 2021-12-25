d10b0bb9fae292708cc4520d616fb5f605dd47f9
asgaraliansari <asgaransariali@gmail.com>
2021-12-25 20:16:11 +0530

    hii i am here

---
+++ src/org/k/GitDemo.java
@@ -0,0 +1,14 @@
+package org.k;
+
+public class GitDemo {
+	public static void main(String[] args) {
+		String s1 ="Greens Technology";
+		String s2 ="Selenium Automation Tool";
+		String s3 ="velmurugan";
+		String s4 ="j a v a p r o g r a m";
+		String s5 ="9848022338";
+		for (int i = 0; i < s1.length(); i++) {
+			System.out.println(i);	
+		}
+	}
+}
